084a4a90554ffb0a71225dd764241dc9e4c227ac
Paul Southall <paulsouthallweb@gmail.com>
2019-01-03 20:30:21 +1000

    Type of example

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
- // Console.log example.
- console.log('hello world2');
+let name = 'Paulio'; // string literal
+let age = 30; // num literal
+let isApproved = true; // bool literal
+let firstName = undefined; // undefined. 
+let lastName = null; // null 
+console.log(firstName); 
