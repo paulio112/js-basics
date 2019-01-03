c71e363dd4814a4bd050cd479d6da674d5ad349b
Paul Southall <paulsouthallweb@gmail.com>
2019-01-03 20:49:59 +1000

    Array example

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-
-//let name = 'default';
-//let age = 50;
-
-let person = {
-    name: 'Paul',
-    age: 30
-};
-
-// Dot notation 
-person.name = 'Steve'
-
-// bracket notation
-let selection = 'name'; 
-//person[] = 'Rodger'
-
-console.log(selection);
+let selectedColours = ['red', 'blue', 'green'];
+console.log(selectedColours[0]);
