4ce5a394aef6dfb8fb83ab5edb6020b73fc8a7b8
Paul Southall <paulsouthallweb@gmail.com>
2019-01-08 20:37:15 +1000

    section 5 - lecture 49 - factory function example.

--- section5.js
+++ section5.js
@@ -1,18 +1,35 @@
 // section 5 - lection 48
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw:function(){
-        console.log('draw method');
-    },
-    move: function(){
-        console.log('move method');
-    }
-}
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+//     draw:function(){
+//         console.log('draw method');
+//     },
+//     move: function(){
+//         console.log('move method');
+//     }
+// }
 
-circle.move();
+// circle.move();
+
+
+// section 5 - lection 49
+
+// factory fucntion 
+
+function createCircle(radius){
+    return {
+        radius,
+
+        draw() {  
+            console.log('draw method');
+        }
+    }; 
+}
+const circle1 = createCircle(1);
+console.log(circle1);
