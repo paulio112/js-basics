7c94ff7c51a5e67612a7fe4ef492b7da0dd1290c
Paul Southall <paulsouthallweb@gmail.com>
2019-01-13 14:20:15 +1000

    section 6: lecture 70 finding primitives in an array

--- section6.js
+++ section6.js
@@ -1,13 +1,20 @@
-//section 6 - lecture 69: adding elements to an array element. 
+// //section 6 - lecture 69: adding elements to an array element. 
 
-const numbers = [3,4];
+// const numbers = [3,4];
 
-//end 
-//numbers.push(4,6);
-//start
+// //end 
+// //numbers.push(4,6);
+// //start
 
-numbers.unshift(1,2);
+// numbers.unshift(1,2);
 
-//middle
-numbers.splice(2,0,'a','b');
-console.log(numbers);
+// //middle
+// numbers.splice(2,0,'a','b');
+// console.log(numbers);
+
+
+// lecture 70 - finding primitives 
+
+const numbers = [1,2,3,1,4]; 
+//console.log(numbers.indexOf(1));
+console.log(numbers.lastIndexOf(1));
