56a607a529363b5be4635130d6cce5f9826be390
Paul Southall <paulsouthallweb@gmail.com>
2019-01-20 14:46:17 +1000

    Finished the beginners course

--- section7.js
+++ section7.js
@@ -57,24 +57,116 @@
 
 // getters and setters example 
 
-const person = {
-    firstName: 'Paul',
-    lastName: 'Southall',
-    set fullName(value) {
-        if (typeof value !== 'string') 
-            throw new Error('Value is not a string');
-        const parts = value.split(' ');
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
-};
+// const person = {
+//     firstName: 'Paul',
+//     lastName: 'Southall',
+//     set fullName(value) {
+//         if (typeof value !== 'string') 
+//             throw new Error('Value is not a string');
+//         const parts = value.split(' ');
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//     }
+// };
 
-// Getters to access properties in an object 
-// setters to change or mutate properties in an object
-try {
-    person.fullname = true; 
-}
-catch(e) {
-    alert(e)
-} 
-console.log(person);
+// // Getters to access properties in an object 
+// // setters to change or mutate properties in an object
+// try {
+//     person.fullname = true; 
+// }
+// catch(e) {
+//     alert(e)
+// } 
+// console.log(person);
+
+
+// Local Vs global scope 
+// function start (){
+//     const message = 'hi';
+// }
+// console.log(message);
+
+
+//let vs var 
+
+// var is function wide open scope -  also adds to the window element.
+// let is block scope. 
+
+
+
+// the this keyword
+
+
+// const video = {
+//     title: 'a', 
+//     tags: ['a','b','c'],
+//     showTags() {
+//         console.log(this.tags);
+//     }
+// };
+
+// video.stop = function() {
+//     console.log(this);
+// }
+
+
+// function Video(title) {
+//     this.title = title; 
+//     console.log(this);
+// }
+
+
+// // playVideo();
+
+// const v = new Video('Hello World');
+
+
+
+// function playVideo() {
+//     console.log(this);
+// };
+
+// playVideo.call({
+//     name:'mossh'
+// });
+
+
+
+// playVideo.apply({
+//     name:'mossh aooky'
+// });
+
+
+
+
+//Exercise 1 - OO programming 
+
+
+// sum() takes lots of arg  and returns the sum 
+// modify sum function to accept array as part 2?
+// array.isArray
+
+// console.log(sum(1,2,3,4));
+
+// function sum (...items) {
+//  if (items.length === 1 && Array.isArray(items][0]))
+//   return items.reduce((a,b) => a + b);
+// }
+
+
+// Circle.radius = 2'
+// circle .area = 20 
+
+// const circle = {
+//     radius:1, 
+//     get area(){
+//         return Math.PI * this.radius * this.radius;
+//     }
+// };
+
+// console.log(circle.area);
+
+
+
+//Exercise 3- error handling. 
+
